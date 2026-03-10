34f80b996ed72fd3b786f1056b12323b2da32c45
Choudhury, Yash <ychoudhury3@gatech.edu>
2026-03-10 18:41:29 -0400

    SOF Parsing complete, mild formatting changes

--- jpeg_decoder.py
+++ jpeg_decoder.py
@@ -4,22 +4,10 @@ import os
 import numpy as np
 from typing import List, Any
 from struct import unpack
-from constants import JFIF_unit_strings, SOF_type_strings
+from constants import JFIF_unit_strings, SOF_type_strings, interleaved_order
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(levelname)s - %(message)s'
-)
 
-interleaved_order =  [[0, 1, 5, 6, 14, 15, 27, 28], 
-                      [2, 4, 7, 13, 16, 26, 29, 42],
-                      [3, 8, 12, 17, 25, 30, 41, 43],
-                      [9, 11, 18, 24, 31, 40, 44, 53],
-                      [10, 19, 23, 32, 39, 45, 52, 54], 
-                      [20, 22, 33, 38, 46, 51, 55, 60], 
-                      [21, 34, 37, 47, 50, 56, 59, 61], 
-                      [35, 36, 48, 49, 57, 58, 62, 63]]
 
 def write_bmp():
     pass
@@ -28,12 +16,12 @@ def write_bmp():
 def get_DQT(DQTs, file_handle):
     DQT = np.zeros((8, 8))
     Lq = int.from_bytes(file_handle.read(2), byteorder='big')
-    
 
-    interleaved_DQT = None
+
     remaining_length = Lq - 2
-    
+
     while remaining_length != 0:
+        interleaved_DQT = None
         PqTq = file_handle.read(1)[0]
         remaining_length -= 1
         Pq = (PqTq >> 4) & 0xf
@@ -50,12 +38,12 @@ def get_DQT(DQTs, file_handle):
 
         logger.debug(
             f"""DQT Segment Length: {Lq}\n"""
-            f"""\tQuantization Precision: {precision_strings[Pq]}\n"""
-            f"""\tTable Identifier: {Tq}"""
+            f"""\t\tQuantization Precision: {precision_strings[Pq]}\n"""
+            f"""\t\tTable Identifier: {Tq}"""
         )
 
         DQT = np.zeros((8, 8))
-        if (Pq == 0):
+        if Pq == 0:
             interleaved_DQT = unpack(">64B", file_handle.read(64))
             remaining_length -= 64
         else:
@@ -65,8 +53,12 @@ def get_DQT(DQTs, file_handle):
             x, y = i // 8, i % 8
             DQT[x][y] = interleaved_DQT[interleaved_order[x][y]]
         DQTs[Tq] = DQT
-def get_DHT(file_handle) -> List[Any]:
+
+
+def get_DHT(DHTs, file_handle):
     pass
+
+
 def read_jpeg(file_name: str):
     cnt = 0
     with open(file_name, "rb") as f:
@@ -75,10 +67,14 @@ def read_jpeg(file_name: str):
             logger.error("incorrect SOI marker")
             exit()
         logger.debug(f"Correct SOI marker. Proceeding.")
-        
+
         DQTs = [[] for _ in range(4)]
-        DHT = None
+        DHTs = None
         SOF_type = None
+        C = []
+        H = []
+        V = []
+        Tq = []
 
         while (True):
             marker = f.read(2)
@@ -86,39 +82,34 @@ def read_jpeg(file_name: str):
                 logger.debug(f"Correct APP0 Marker. Proceeding.")
                 APP0_length = int.from_bytes(f.read(2), byteorder='big')
                 logger.debug(f"APP0 length read as {APP0_length}")
-                
-                f.seek(-2, os.SEEK_CUR) # move back to start of APP0 header
-                APP0 = f.read(APP0_length)
-                
-                APP0_length = int.from_bytes(APP0[0:2], byteorder='big')
-                JFIF_identifier = APP0[2:7].decode('ascii')
-                JFIF_major_ver = APP0[7]
-                JFIF_minor_ver = APP0[8]
-                units = APP0[9]
-                Xdensity = int.from_bytes(APP0[10:12])
-                Ydensity = int.from_bytes(APP0[12:14])
-                Xthumbnail = APP0[14]
-                Ythumbnail = APP0[15]
 
-                if (JFIF_identifier != "JFIF\0"):
-                    logger.warn(f"JFIF Identifier \"{JFIF_identifier}\" != \"JFIF\\0\"")
-                if (JFIF_major_ver != 1):
-                    logger.warn(f"JFIF Major Version {JFIF_major_ver} != 1")
+                f.seek(-2, os.SEEK_CUR)  # move back to start of APP0 header
+                APP0 = f.read(APP0_length)
+                APP0_length, JFIF_identifier, JFIF_major_ver, JFIF_minor_ver, \
+                    units, Xdensity, Ydensity, Xthumbnail, Ythumbnail = unpack(
+                    ">H5sBBBHHBB",
+                    APP0
+                )
+
+                if JFIF_identifier != "JFIF\0":
+                    logger.warning(f"JFIF Identifier \"{JFIF_identifier}\" != \"JFIF\\0\"")
+                if JFIF_major_ver != 1:
+                    logger.warning(f"JFIF Major Version {JFIF_major_ver} != 1")
                 if not 0 <= JFIF_minor_ver <= 2:
-                    logger.warn(f"JFIF Minor Version {JFIF_minor_ver} not in range .00-.02")
+                    logger.warning(f"JFIF Minor Version {JFIF_minor_ver} not in range .00-.02")
                 if not 0 <= units <= 2:
-                    logger.warn(f"Invalid units specifier {units}")
+                    logger.warning(f"Invalid units specifier {units}")
 
                 logger.debug(
                     f"APP0 Parsed as\n"
-                    f"\tAPP0 length: {APP0_length}\n"
-                    f"\tJFIF identifier: \"{JFIF_identifier}\"\n"
-                    f"\tJFIF version: {JFIF_major_ver}.0{JFIF_minor_ver}\n"
-                    f"\tX, Y units: {JFIF_unit_strings[units]}\n"
-                    f"\tXdensity: {Xdensity}\n"
-                    f"\tYdensity: {Ydensity}\n"
-                    f"\tXthumbnail: {Xthumbnail}\n"
-                    f"\tYthumbnail: {Ythumbnail}"
+                    f"\t\tAPP0 length: {APP0_length}\n"
+                    f"\t\tJFIF identifier: \"{JFIF_identifier}\"\n"
+                    f"\t\tJFIF version: {JFIF_major_ver}.0{JFIF_minor_ver}\n"
+                    f"\t\tX, Y units: {JFIF_unit_strings[units]}\n"
+                    f"\t\tXdensity: {Xdensity}\n"
+                    f"\t\tYdensity: {Ydensity}\n"
+                    f"\t\tXthumbnail: {Xthumbnail}\n"
+                    f"\t\tYthumbnail: {Ythumbnail}"
                 )
             elif marker == b'\xFF\xE1':
                 logger.warning("EXIF detected! Header Parsing not supported!")
@@ -130,40 +121,71 @@ def read_jpeg(file_name: str):
                 logger.debug("DQT marker found! Parsing DQT!")
                 get_DQT(DQTs, f)
             elif marker == b'\xFF\xC4':
-                DHT = get_DHT(f)
+                get_DHT(DHTs, f)
                 logger.error("DHT marker found! DHT based encoding methods not supported!")
                 exit()
             elif b'\xFF\xC0' <= marker <= b'\xFF\xCF':
                 SOF_type = marker[1] - 0xC0
-                break
+                SOF_header = f.read(8)
+                Lf, P, Y, X, Nf = unpack(">HBHHB", SOF_header)
+
+                logger.debug(
+                    f"SOF Header Type: {SOF_type_strings[SOF_type]}\n"
+                    f"\t\tHeader Length: {Lf}\n"
+                    f"\t\tSample Precision: {P}\n"
+                    f"\t\tNumber of Lines: {Y}\n"
+                    f"\t\tSamples per Line: {X}\n"
+                    f"\t\tNumber of Image Components: {Nf}"
+                )
+
+                for i in range(Nf):
+                    Ci, HiVi, Tqi = unpack(">BBB", f.read(3))
+                    Hi = HiVi >> 4
+                    Vi = HiVi & 0xF
+                    if not 1 <= Hi <= 4:
+                        error_str = f"Invalid H{i} value: {Hi}"
+                        logger.error(error_str)
+                        return
+                    if not 1 <= Vi <= 4:
+                        error_str = f"Invalid V{i} value: {Vi}"
+                        logger.error(error_str)
+                        return
+                    if not 0 <= Tqi <= 3:
+                        error_str = f"Invalid Tq{i} value: {Tqi}"
+                        logger.error(error_str)
+                        return
+                    C.append(Ci)
+                    H.append(Hi)
+                    V.append(Vi)
+                    Tq.append(Tqi)
+
+                    logger.debug(f"SOF Component {i}\n"
+                                 f"\t\tComponent Identifer: {Ci}\n"
+                                 f"\t\tHorizontal Sampling Factor: {Hi}\n"
+                                 f"\t\tVertical Sampling Factor: {Vi}\n"
+                                 f"\t\tQuantization Table Destination: {Tqi}"
+                                 )
             else:
                 logger.error(f"Unsupported segment marker: \"{marker.hex()}\"!")
-                raise NotImplementedError(f"Unsupported segment marker: \'{marker.hex(sep='\'').upper()}!")
-        
-        component_specific_headers = []
+                return
+
         # begin SOF parsing
-
-        SOF_header = f.read(8)
-        Lf = int.from_bytes(SOF_header[0:2], byteorder='big')
-        P = SOF_header[2]
-        Y = int.from_bytes(SOF_header[3:5], byteorder='big')
-        X = int.from_bytes(SOF_header[5:7], byteorder='big')
-        Nf = SOF_header[7]
-
-        logger.debug(
-                    f"SOF Header Type: {SOF_type_strings[SOF_type]}\n"
-                    f"\tHeader Length: {Lf}\n"
-                    f"\tSample Precision: {P}\n"
-                    f"\tNumber of Lines: {Y}\n"
-                    f"\tSamples per Line: {X}\n"
-                    f"\tNumber of Image Components: {Nf}"
-        )            
-
-        
-
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
+parser.add_argument('-debug', '-g', action='store_true')
 args = parser.parse_args()
+
+if args.debug:
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(levelname)s - %(message)s'
+    )
+else:
+    logging.basicConfig(
+        level=logging.WARNING,
+        format='%(levelname)s - %(message)s'
+    )
+
 read_jpeg(args.filename)
